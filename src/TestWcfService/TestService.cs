d0359c7653f30e219aeab79247610014675c1045
Gianluca Bertelli <gianluca.bertelli@live.it>
2016-09-20 14:13:17 +0200

    Impersonate example (commented)

--- src/TestWcfService/TestService.cs
+++ src/TestWcfService/TestService.cs
@@ -25,6 +25,31 @@ namespace TestWcfService
                 builder.AppendLine("ClaimType :" + claim.Type + "   ClaimValue:" + claim.Value);
             }
 
+
+            //Impersonation Example
+            //----------------------
+            //if we want to impersonate we need the UPN as to be able to create a Windows Identity
+            //then we should run this service as LOCAL SYSTEM to have all the privileges (psexec -s TestWcfService.exe)
+            //and create a c:\temp folder. The file created will be written with the caller privileges and properties (i.e: check the security tab and look for owner field)
+
+            //var upnClaim = claimsPrincipal.Claims.Where((c) => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn").FirstOrDefault();
+            //if(upnClaim == null)
+            //    throw new InvalidOperationException("The caller has not an UPN claim");
+
+            //WindowsIdentity callerWindowsIdentity = new WindowsIdentity(upnClaim.Value);
+            //if (callerWindowsIdentity == null)
+            //{
+            //    throw new InvalidOperationException("The caller cannot be mapped to a WindowsIdentity");
+            //}
+            //using (callerWindowsIdentity.Impersonate())
+            //{
+            //    // Access a file as the caller.
+            //    var f = System.IO.File.CreateText(@"c:\temp\test.txt");
+            //    f.WriteLine("Hello");
+            //    f.Close();
+            //}
+
+
             return builder.ToString();
         }
     }
